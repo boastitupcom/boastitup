d3ea3b24c1fed9f03aeb84e140efce62bdbd8e35
smartgenai <genaiworkggn@gmail.com>
2025-11-24 10:58:29 +0530

    badge

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,7 +1,6 @@
 // Base UI Components
 export * from "./src/components/ui/card";
 export * from "./src/components/ui/button";
-export * from "./src/components/ui/badge";
 export * from "./src/components/ui/input";
 export * from "./src/components/ui/label";
 export * from "./src/components/ui/scroll-area";
